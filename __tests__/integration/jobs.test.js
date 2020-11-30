38c86d7cefd1707595ed07fb23b5a6d7276eeb76
lafBot <64456835+lafBot@users.noreply.github.com>
2020-11-29 20:31:41 -0700

    Updated Job route tests to include authorization

--- __tests__/integration/jobs.test.js
+++ __tests__/integration/jobs.test.js
@@ -2,10 +2,14 @@ process.env.NODE_ENV = "TEST";
 const request = require("supertest");
 const app = require("../../app");
 const db = require("../../db");
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { BCRYPT_WORK_FACTOR } = require('../../config');
 
 let company1;
 let job1;
 let job2;
+let user1;
 
 beforeEach(async () => {
     let resp = await db.query(`
@@ -38,11 +42,33 @@ beforeEach(async () => {
     RETURNING
     id, title, salary, equity, company_handle, date_posted`);
     job2 = resp3.rows[0];
+
+    const password = await bcrypt.hash('Password1', BCRYPT_WORK_FACTOR);
+
+    let user = await db.query(`
+    INSERT INTO users
+    (username, password, first_name, last_name, email, photo_url, is_admin)
+    VALUES
+    ('testUser', $1, 'Bob', 'Ross', 'BobRossArt@gmail.com', 'bob_art_url.com', True)
+    RETURNING
+    username, password, first_name, last_name, email, photo_url, is_admin
+    `, [password]);
+
+    const login = await request(app)
+        .post('/login')
+        .send({
+            username: "testUser",
+            password: "Password1"
+        });
+    
+    user1 = user.rows[0];
+    user1.userToken = login.body.token;
+    user1.currentUsername = jwt.decode(user1.userToken).username;
 })
 
 describe('GET /jobs', () => {
     test('Get all jobs', async () => {
-        const resp = await request(app).get(`/jobs`);
+        const resp = await request(app).get(`/jobs`).send({ _token: `${user1.userToken}` });
 
         expect(resp.statusCode).toBe(200);
         expect(resp.body.jobs[0]).toHaveProperty("title");
@@ -51,7 +77,7 @@ describe('GET /jobs', () => {
     });
 
     test('Get jobs with minimum salary of 90K', async () => {
-        const resp = await request(app).get(`/jobs?min_salary=90000`);
+        const resp = await request(app).get(`/jobs?min_salary=90000`).send({ _token: `${user1.userToken}` });
 
         expect(resp.statusCode).toBe(200);
         expect(resp.body.jobs[0]).toHaveProperty("title");
@@ -59,7 +85,7 @@ describe('GET /jobs', () => {
     });
 
     test('Get jobs with minimum equity position of 0.05', async () => {
-        const resp = await request(app).get(`/jobs?min_equity=0.03`);
+        const resp = await request(app).get(`/jobs?min_equity=0.03`).send({ _token: `${user1.userToken}` });
 
         expect(resp.statusCode).toBe(200);
         expect(resp.body.jobs[0]).toHaveProperty("title");
@@ -67,7 +93,7 @@ describe('GET /jobs', () => {
     });
 
     test('Get jobs with title "Salesman"', async () => {
-        const resp = await request(app).get(`/jobs?search=Salesman`);
+        const resp = await request(app).get(`/jobs?search=Salesman`).send({ _token: `${user1.userToken}` });
 
         expect(resp.statusCode).toBe(200);
         expect(resp.body.jobs.length).toBe(1);
@@ -78,7 +104,7 @@ describe('GET /jobs', () => {
 
 describe('GET /jobs/:id', () => {
     test('Get specific job posting by id', async () => {
-        const resp = await request(app).get(`/jobs/${job1.id}`)
+        const resp = await request(app).get(`/jobs/${job1.id}`).send({ _token: `${user1.userToken}` });
 
         expect(resp.statusCode).toBe(200);
         expect(resp.body.job).toHaveProperty("title");
@@ -95,7 +121,8 @@ describe('POST /jobs', () => {
                 "title": "Sales Assistant",
                 "salary": 40000.01,
                 "equity": 0.02,
-                "company_handle": company1.handle
+                "company_handle": company1.handle,
+                _token: `${user1.userToken}`
         });
 
         expect(resp.statusCode).toBe(201);
@@ -111,17 +138,19 @@ describe('PATCH /jobs', () => {
     test('Allows for edit to job info', async () => {
         const resp = await request(app).patch(`/jobs/${job1.id}`).send({
             "title": "Sales Manager",
-            "salary": 200000
+            "salary": 200000,
+            _token: `${user1.userToken}`
         });
         expect(resp.statusCode).toBe(200);
         expect(resp.body.job.title).toBe("Sales Manager");
         expect(resp.body.job.salary).toBe(200000);
     });
 
-    test('Throws error for invalid edit to company info', async () => {
+    test('Throws error for invalid edit to job info', async () => {
         const resp = await request(app).patch(`/companies/${job1.id}`).send({
             "title": 9000,
-            "salary": "Invalid"
+            "salary": "Invalid",
+            _token: `${user1.userToken}`
         });
 
         expect(resp.statusCode).toBe(400);
@@ -130,22 +159,23 @@ describe('PATCH /jobs', () => {
 
 describe('DELETE /job', () => {
     test('Deletes a specified job', async () => {
-        const resp = await request(app).delete(`/jobs/${job1.id}`);
+        const resp = await request(app).delete(`/jobs/${job1.id}`).send({ _token: `${user1.userToken}` });
 
         expect(resp.statusCode).toBe(200);
         expect(resp.body.message).toBe("Job deleted");
     });
 
     test('Returns error for invalid job DELETE request', async () => {
-        const resp = await request(app).delete(`/jobs/1`);
+        const resp = await request(app).delete(`/jobs/1`).send({ _token: `${user1.userToken}` });
 
         expect(resp.statusCode).toBe(404);
     });
 });
 
 afterEach(async () => {
-    await db.query('DELETE FROM companies')
-    await db.query('DELETE FROM jobs')
+    await db.query('DELETE FROM companies');
+    await db.query('DELETE FROM jobs');
+    await db.query('DELETE FROM users');
 })
 
 afterAll(async () => {
